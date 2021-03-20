08419ab9994f7c0dbe5bfe5f28740a21ec3aeca3
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-03-21 08:57:37 +0900

    fix: add test

--- test/OpenJpegDotNet.Tests/OpenJpegTest.cs
+++ test/OpenJpegDotNet.Tests/OpenJpegTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using OpenJpegDotNet;
@@ -33,7 +34,45 @@ namespace OpenJpegDotNet.Tests
 
         #region Image
 
+        [Fact]
+        public void ToBitmap()
+        {
+            var targets = new[]
+            {
+                //new { Name = "Bretagne1_0.j2k", IsReadStream = true, Format = CodecFormat.Unknown,  Result = false },
+                new { Name = "Bretagne1_0.j2k", IsReadStream = true, Format = CodecFormat.J2k,      Result = true  },
+                //new { Name = "Bretagne1_0.j2k", IsReadStream = true, Format = CodecFormat.Jp2,      Result = false  },
+                //new { Name = "Bretagne1_0.j2k", IsReadStream = true, Format = CodecFormat.Jpp,      Result = false },
+                //new { Name = "Bretagne1_0.j2k", IsReadStream = true, Format = CodecFormat.Jpt,      Result = false },
+                //new { Name = "Bretagne1_0.j2k", IsReadStream = true, Format = CodecFormat.Jpx,      Result = false }
+            };
 
+            foreach (var target in targets)
+            {
+                var path = Path.GetFullPath(Path.Combine(TestImageDirectory, target.Name));
+
+                var stream = OpenJpeg.StreamCreateDefaultFileStream(path, target.IsReadStream);
+                var codec = OpenJpeg.CreateDecompress(target.Format);
+                var decompressionParameters = new DecompressionParameters();
+                OpenJpeg.SetDefaultDecoderParameters(decompressionParameters);
+                Assert.True(OpenJpeg.SetupDecoder(codec, decompressionParameters) == target.Result, $"Failed to invoke {nameof(OpenJpeg.SetupDecoder)} for {target.Format} and {target.IsReadStream}");
+                Assert.True(OpenJpeg.ReadHeader(stream, codec, out var image) == target.Result, $"Failed to invoke {nameof(OpenJpeg.ReadHeader)} for {target.Format} and {target.IsReadStream}");
+                Assert.True(OpenJpeg.SetDecodeArea(codec, image, 0, 0, 0, 0) == target.Result, $"Failed to invoke {nameof(OpenJpeg.SetDecodeArea)} for {target.Format} and {target.IsReadStream}");
+                Assert.True(OpenJpeg.Decode(codec, stream, image) == target.Result, $"Failed to invoke {nameof(OpenJpeg.Decode)} for {target.Format} and {target.IsReadStream}");
+                Assert.True(OpenJpeg.EndDecompress(codec, stream) == target.Result, $"Failed to invoke {nameof(OpenJpeg.EndDecompress)} for {target.Format} and {target.IsReadStream}");
+
+                using (var bitmap = image.ToBitmap())
+                {
+                    var bitmapPath = Path.ChangeExtension(path, "bmp");
+                    bitmap.Save(bitmapPath, ImageFormat.Bmp);
+                }
+
+                this.DisposeAndCheckDisposedState(image);
+                this.DisposeAndCheckDisposedState(stream);
+                this.DisposeAndCheckDisposedState(decompressionParameters);
+                this.DisposeAndCheckDisposedState(codec);
+            }
+        }
 
         #endregion
 
@@ -178,14 +217,14 @@ namespace OpenJpegDotNet.Tests
                 OpenJpeg.SetDefaultDecoderParameters(decompressionParameters);
                 var ret = OpenJpeg.SetupDecoder(codec, decompressionParameters);
                 Assert.True(OpenJpeg.ReadHeader(stream, codec, out var image) == target.Result, $"Failed to invoke {nameof(OpenJpeg.ReadHeader)} for {target.Format} and {target.IsReadStream}");
-                
+
                 this.DisposeAndCheckDisposedState(image);
                 this.DisposeAndCheckDisposedState(stream);
                 this.DisposeAndCheckDisposedState(decompressionParameters);
                 this.DisposeAndCheckDisposedState(codec);
             }
         }
-        
+
         [Fact]
         public void SetDecodeArea()
         {
