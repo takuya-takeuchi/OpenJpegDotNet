3508c0439554be5d449ece67f07d808e18d28285
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-05-08 04:32:15 +0900

    fix

--- test/OpenJpegDotNet.UWP.Tests/MainPage.xaml.cs
+++ test/OpenJpegDotNet.UWP.Tests/MainPage.xaml.cs
@@ -1,30 +1,24 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Threading.Tasks;
 using Windows.Graphics.Imaging;
 using Windows.Storage.Streams;
-using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
-using OpenJpegDotNet;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
 namespace OpenJpegDotNet.UWP.Tests
 {
+
     /// <summary>
     /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
     /// </summary>
     public sealed partial class MainPage : Page
     {
-
-        #region Fields
-
-        private Matrix<RgbPixel> _LoadedMatrix;
-
-        private Array2D<RgbPixel> _Result;
-
-        #endregion
 
         #region Constructors
 
@@ -41,69 +35,80 @@ namespace OpenJpegDotNet.UWP.Tests
 
         private async void LoadButtonClick(object sender, RoutedEventArgs e)
         {
-            this._Result?.Dispose();
-            this._LoadedMatrix?.Dispose();
-
-            this._Result = Dlib.LoadImage<RgbPixel>("lenna.bmp");
-            this._LoadedMatrix = new Matrix<RgbPixel>(this._Result);
-
-            this._Image.Source = await ToBitmapSource(this._LoadedMatrix);
-        }
-
-        private async void DetectButtonClick(object sender, RoutedEventArgs e)
-        {
-            using (var faceDetector = Dlib.GetFrontalFaceDetector())
+            var data = await File.ReadAllBytesAsync("Bretagne1_0.j2k");
+            using (var reader = new IO.Reader(data))
             {
-                var dets = faceDetector.Operator(this._LoadedMatrix);
-                foreach (var r in dets)
-                    Dlib.DrawRectangle(this._Result, r, new RgbPixel { Green = 255 });
+                var result = reader.ReadHeader();
+                if (!result)
+                    return;
+
+                using (var bitmap = reader.ReadData())
+                    this._Image.Source = await ToBitmapSource(bitmap);
             }
-
-            BitmapImage image;
-            using (var tmp = new Matrix<RgbPixel>(this._Result))
-                image = await ToBitmapSource(tmp);
-
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                this._Image.Source = image;
-            });
         }
 
         #endregion
 
-        #region Helpers
+        #region Helperss
 
-        private static async Task<BitmapImage> ToBitmapSource(Matrix<RgbPixel> matrix)
+        private static async Task<BitmapImage> ToBitmapSource(Bitmap bitmap)
         {
-            var width = matrix.Columns;
-            var height = matrix.Rows;
+            var width = bitmap.Width;
+            var height = bitmap.Height;
+            var pixelFormat = bitmap.PixelFormat;
 
-            var bitmap = new WriteableBitmap(width, height);
-            using (var imras = new InMemoryRandomAccessStream())
+            BitmapData bitmapData = null;
+
+            try
             {
-                var array = matrix.ToArray();
-                var data = new byte[array.Length * 4];
-                for (int d = 0, s = 0; d < data.Length; d += 4, s++)
+                var rect = new Rectangle(0, 0, width, height);
+                bitmapData = bitmap.LockBits(rect, ImageLockMode.ReadOnly, pixelFormat);
+                
+                var data = new byte[width * height * 4];
+
+                unsafe
                 {
-                    data[d] = array[s].Blue;
-                    data[d + 1] = array[s].Green;
-                    data[d + 2] = array[s].Red;
+                    var src = (byte*)bitmapData.Scan0;
+                    fixed (byte* dst = &data[0])
+                    {
+                        for (var y = 0; y < height; y++)
+                        {
+                            var index = y * width * 4;
+                            for (var x = 0; x < width; x++)
+                            {
+                                dst[index] = *++src;
+                                dst[index + 1] = *++src;
+                                dst[index + 2] = *++src;
+                                ++src;
+                                index += 4;
+                            }
+                        }
+                    }
                 }
+                var writeableBitmap = new WriteableBitmap(width, height);
+                using (var imras = new InMemoryRandomAccessStream())
+                {
 
-                var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, imras);
-                encoder.SetPixelData(BitmapPixelFormat.Bgra8,
-                    BitmapAlphaMode.Ignore,
-                    (uint)bitmap.PixelWidth,
-                    (uint)bitmap.PixelHeight,
-                    96.0,
-                    96.0,
-                    data);
+                    var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, imras);
+                    encoder.SetPixelData(BitmapPixelFormat.Bgra8,
+                                         BitmapAlphaMode.Ignore,
+                                         (uint)writeableBitmap.PixelWidth,
+                                         (uint)writeableBitmap.PixelHeight,
+                                         96.0,
+                                         96.0,
+                                         data);
 
-                await encoder.FlushAsync();
-                var bitmapImage = new BitmapImage();
-                bitmapImage.SetSource(imras);
+                    await encoder.FlushAsync();
+                    var bitmapImage = new BitmapImage();
+                    bitmapImage.SetSource(imras);
 
-                return bitmapImage;
+                    return bitmapImage;
+                }
+            }
+            finally
+            {
+                if (bitmapData != null)
+                    bitmap.UnlockBits(bitmapData);
             }
         }
 
@@ -112,4 +117,5 @@ namespace OpenJpegDotNet.UWP.Tests
         #endregion
 
     }
+
 }
