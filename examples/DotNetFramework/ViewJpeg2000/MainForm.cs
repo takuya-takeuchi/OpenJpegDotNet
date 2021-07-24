1bf74e69a84fcbbaafbd55d3f1127541d565376b
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-07-25 01:48:01 +0900

    fix

---
+++ examples/DotNetFramework/ViewJpeg2000/MainForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace ViewJpeg2000
+{
+
+    public partial class MainForm : Form
+    {
+
+        #region Constructors
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.textBox.Text = "https://github.com/takuya-takeuchi/OpenJpegDotNet/blob/main/test/OpenJpegDotNet.Tests/TestImages/Bretagne1_0.j2k?raw=true";
+        }
+
+        #endregion
+
+        #region Methods
+
+        #region Event Handlers
+
+        private async void ButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                var webClient = new WebClient();
+                var content = await webClient.DownloadDataTaskAsync(new Uri(this.textBox.Text));
+
+                using (var reader = new OpenJpegDotNet.IO.Reader(content))
+                {
+                    var result = reader.ReadHeader();
+                    this.pictureBox.Image?.Dispose();
+                    this.pictureBox.Image = reader.ReadData();
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+
+}
---
+++ examples/DotNetFramework/ViewJpeg2000/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("ViewJpeg2000")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ViewJpeg2000")]
+[assembly: AssemblyCopyright("Copyright © Takuya Takeuchi 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("fe8b05a4-5d47-4523-96f9-4188738bcb51")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
